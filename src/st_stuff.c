c0083977846199d27cd95a32f7c6711fcafee5d3
Jaden <jadenarcalt@gmail.com>
2022-05-18 17:30:46 -0500

    Add V_PERPLAYER flags to ST_drawInput

--- src/st_stuff.c
+++ src/st_stuff.c
@@ -988,12 +988,14 @@ static void ST_drawInput(void)
 	if (!cv_showinput.value) // dont draw this if showinput is disabled! 
 		return;
 
-	const INT32 accent = V_SNAPTOLEFT|V_SNAPTOBOTTOM|(stplyr->skincolor ? skincolors[stplyr->skincolor].ramp[4] : 0);
-	INT32 col;
-	UINT8 offs;
-
 	INT32 x = hudinfo[HUD_LIVES].x;
 	INT32 y = hudinfo[HUD_LIVES].y;
+	
+	UINT32 flags = hudinfo[HUD_LIVES].f|V_PERPLAYER;
+
+	const INT32 accent = flags|(stplyr->skincolor ? skincolors[stplyr->skincolor].ramp[4] : 0);
+	INT32 col;
+	UINT8 offs;
 
 	// offset the height if we arent on record attack,
 	if (!modeattacking)
@@ -1007,8 +1009,8 @@ static void ST_drawInput(void)
 		return;
 
 	// O backing
-	V_DrawFill(x, y-1, 16, 16, hudinfo[HUD_LIVES].f|20);
-	V_DrawFill(x, y+15, 16, 1, hudinfo[HUD_LIVES].f|29);
+	V_DrawFill(x, y-1, 16, 16, flags|20);
+	V_DrawFill(x, y+15, 16, 1, flags|29);
 
 	if (cv_showinputjoy.value) // joystick render!
 	{
@@ -1019,11 +1021,11 @@ static void ST_drawInput(void)
 		if (stplyr->cmd.sidemove || stplyr->cmd.forwardmove)
 		{
 			// joystick hole
-			V_DrawFill(x+5, y+4, 6, 6, hudinfo[HUD_LIVES].f|29);
+			V_DrawFill(x+5, y+4, 6, 6, flags|29);
 			// joystick top
 			V_DrawFill(x+3+stplyr->cmd.sidemove/12,
 				y+2-stplyr->cmd.forwardmove/12,
-				10, 10, hudinfo[HUD_LIVES].f|29);
+				10, 10, flags|29);
 			V_DrawFill(x+3+stplyr->cmd.sidemove/9,
 				y+1-stplyr->cmd.forwardmove/9,
 				10, 10, accent);
@@ -1031,10 +1033,10 @@ static void ST_drawInput(void)
 		else
 		{
 			// just a limited, greyed out joystick top
-			V_DrawFill(x+3, y+11, 10, 1, hudinfo[HUD_LIVES].f|29);
+			V_DrawFill(x+3, y+11, 10, 1, flags|29);
 			V_DrawFill(x+3,
 				y+1,
-				10, 10, hudinfo[HUD_LIVES].f|16);
+				10, 10, flags|16);
 		}
 	}
 	else // arrows!
@@ -1048,10 +1050,10 @@ static void ST_drawInput(void)
 		else
 		{
 			offs = 1;
-			col = hudinfo[HUD_LIVES].f|16;
-			V_DrawFill(x- 2, y+10,  6,  1, hudinfo[HUD_LIVES].f|29);
-			V_DrawFill(x+ 4, y+ 9,  1,  1, hudinfo[HUD_LIVES].f|29);
-			V_DrawFill(x+ 5, y+ 8,  1,  1, hudinfo[HUD_LIVES].f|29);
+			col = flags|16;
+			V_DrawFill(x- 2, y+10,  6,  1, flags|29);
+			V_DrawFill(x+ 4, y+ 9,  1,  1, flags|29);
+			V_DrawFill(x+ 5, y+ 8,  1,  1, flags|29);
 		}
 		V_DrawFill(x- 2, y+ 5-offs,  6,  6, col);
 		V_DrawFill(x+ 4, y+ 6-offs,  1,  4, col);
@@ -1066,12 +1068,12 @@ static void ST_drawInput(void)
 		else
 		{
 			offs = 1;
-			col = hudinfo[HUD_LIVES].f|16;
-			V_DrawFill(x+ 5, y+ 3,  1,  1, hudinfo[HUD_LIVES].f|29);
-			V_DrawFill(x+ 6, y+ 4,  1,  1, hudinfo[HUD_LIVES].f|29);
-			V_DrawFill(x+ 7, y+ 5,  2,  1, hudinfo[HUD_LIVES].f|29);
-			V_DrawFill(x+ 9, y+ 4,  1,  1, hudinfo[HUD_LIVES].f|29);
-			V_DrawFill(x+10, y+ 3,  1,  1, hudinfo[HUD_LIVES].f|29);
+			col = flags|16;
+			V_DrawFill(x+ 5, y+ 3,  1,  1, flags|29);
+			V_DrawFill(x+ 6, y+ 4,  1,  1, flags|29);
+			V_DrawFill(x+ 7, y+ 5,  2,  1, flags|29);
+			V_DrawFill(x+ 9, y+ 4,  1,  1, flags|29);
+			V_DrawFill(x+10, y+ 3,  1,  1, flags|29);
 		}
 		V_DrawFill(x+ 5, y- 2-offs,  6,  6, col);
 		V_DrawFill(x+ 6, y+ 4-offs,  4,  1, col);
@@ -1086,10 +1088,10 @@ static void ST_drawInput(void)
 		else
 		{
 			offs = 1;
-			col = hudinfo[HUD_LIVES].f|16;
-			V_DrawFill(x+12, y+10,  6,  1, hudinfo[HUD_LIVES].f|29);
-			V_DrawFill(x+11, y+ 9,  1,  1, hudinfo[HUD_LIVES].f|29);
-			V_DrawFill(x+10, y+ 8,  1,  1, hudinfo[HUD_LIVES].f|29);
+			col = flags|16;
+			V_DrawFill(x+12, y+10,  6,  1, flags|29);
+			V_DrawFill(x+11, y+ 9,  1,  1, flags|29);
+			V_DrawFill(x+10, y+ 8,  1,  1, flags|29);
 		}
 		V_DrawFill(x+12, y+ 5-offs,  6,  6, col);
 		V_DrawFill(x+11, y+ 6-offs,  1,  4, col);
@@ -1104,8 +1106,8 @@ static void ST_drawInput(void)
 		else
 		{
 			offs = 1;
-			col = hudinfo[HUD_LIVES].f|16;
-			V_DrawFill(x+ 5, y+17,  6,  1, hudinfo[HUD_LIVES].f|29);
+			col = flags|16;
+			V_DrawFill(x+ 5, y+17,  6,  1, flags|29);
 		}
 		V_DrawFill(x+ 5, y+12-offs,  6,  6, col);
 		V_DrawFill(x+ 6, y+11-offs,  4,  1, col);
@@ -1121,16 +1123,16 @@ static void ST_drawInput(void)
 	else\
 	{\
 		offs = 1;\
-		col = hudinfo[HUD_LIVES].f|16;\
-		V_DrawFill(x+16+(xoffs), y+9+(yoffs), 10, 1, hudinfo[HUD_LIVES].f|29);\
+		col = flags|16;\
+		V_DrawFill(x+16+(xoffs), y+9+(yoffs), 10, 1, flags|29);\
 	}\
 	V_DrawFill(x+16+(xoffs), y+(yoffs)-offs, 10, 10, col);\
-	V_DrawCharacter(x+16+1+(xoffs), y+1+(yoffs)-offs, hudinfo[HUD_LIVES].f|symb, false)
+	V_DrawCharacter(x+16+1+(xoffs), y+1+(yoffs)-offs, flags|symb, false)
 
 	drawbutt( 4,-3, BT_JUMP, 'J');
 	drawbutt(15,-3, BT_SPIN, 'S');
 
-	V_DrawFill(x+16+4, y+8, 21, 10, hudinfo[HUD_LIVES].f|20); // sundial backing
+	V_DrawFill(x+16+4, y+8, 21, 10, flags|20); // sundial backing
 	if (stplyr->mo)
 	{
 		UINT8 i, precision;
@@ -1150,7 +1152,7 @@ static void ST_drawInput(void)
 		{
 			V_DrawFill(x+16+14-(i*xcomp)/precision,
 				y+12-(i*ycomp)/precision,
-				1, 1, hudinfo[HUD_LIVES].f|16);
+				1, 1, flags|16);
 		}
 
 		if (ycomp <= 0)
@@ -1170,7 +1172,7 @@ static void ST_drawInput(void)
 			if (stplyr->pflags & PF_AUTOBRAKE)
 			{
 				V_DrawThinString(x, y,
-					hudinfo[HUD_LIVES].f|
+					flags|
 					((!stplyr->powers[pw_carry]
 					&& (stplyr->pflags & PF_APPLYAUTOBRAKE)
 					&& !(stplyr->cmd.sidemove || stplyr->cmd.forwardmove)
@@ -1183,22 +1185,22 @@ static void ST_drawInput(void)
 			switch (P_ControlStyle(stplyr))
 			{
 			case CS_LMAOGALOG:
-				V_DrawThinString(x, y, hudinfo[HUD_LIVES].f, "ANALOG");
+				V_DrawThinString(x, y, flags, "ANALOG");
 				y -= 8;
 				break;
 
 			case CS_SIMPLE:
-				V_DrawThinString(x, y, hudinfo[HUD_LIVES].f, "AUTOMATIC");
+				V_DrawThinString(x, y, flags, "AUTOMATIC");
 				y -= 8;
 				break;
 
 			case CS_STANDARD:
-				V_DrawThinString(x, y, hudinfo[HUD_LIVES].f, "MANUAL");
+				V_DrawThinString(x, y, flags, "MANUAL");
 				y -= 8;
 				break;
 
 			case CS_LEGACY:
-				V_DrawThinString(x, y, hudinfo[HUD_LIVES].f, "STRAFE");
+				V_DrawThinString(x, y, flags, "STRAFE");
 				y -= 8;
 				break;
 
@@ -1207,7 +1209,7 @@ static void ST_drawInput(void)
 			}
 		}
 		if (!demosynced) // should always be last, so it doesn't push anything else around
-			V_DrawThinString(x, y, hudinfo[HUD_LIVES].f|((leveltime & 4) ? V_YELLOWMAP : V_REDMAP), "BAD DEMO!!");
+			V_DrawThinString(x, y, flags|((leveltime & 4) ? V_YELLOWMAP : V_REDMAP), "BAD DEMO!!");
 	}
 }
 
